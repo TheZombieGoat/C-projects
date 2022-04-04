495bfe971a862f52bf749aa14def080d4be8aaeb
TheZombieGoat <98498075+TheZombieGoat@users.noreply.github.com>
2022-04-04 01:24:04 -0400

    Update partition.c

--- partition.c
+++ partition.c
@@ -1,43 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-int partition(int arr[], int narr)
+void swap(int *a, int *b) //helper function
 {
-	int pivot = arr[0];
-	int j = 0;
 	int temp = 0;
-	int k = narr-1;
-	int same = 0; 
-
-	for (int i = 0; i < narr; i++)
-	{	
-		if(arr[i] < pivot){
-			arr[j] = arr[i];
-			j++;
-		}else if(arr[i] > pivot && k > i){
-			temp = arr[k];
-			arr[k] = arr[i];
-			arr[i] = temp;
-			k--;
-			i--;
-		}
-
-		if(i == narr-1)
-			arr[j] = pivot;
-	}
-	return j;
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
+
+int partition(int arr[], int l, int r)
+{
+	int temp = 0;
+	int j = l;
+	int pivot = arr[l];
+
+	for(int i = l; i < r; i++){
+
+		if(arr[i] <= pivot){
+			swap(&arr[i], &arr[j]);
+			j++;
+		}
+	}
+	swap(&arr[j-1], &arr[l]);
+	return j-1;
+}
+
+int partsort(int arr[], int l, int r)
+{
+	if(l < r){
+		int z = partition(arr,l,r);
+		partsort(arr,l,z);
+		partsort(arr,z+1,r);
+	}
+}
+
 
 int main()
 {
-	int arr[30] = {7,2,3,4,5,6,1,8,9,2,11,12,13,55,44,4,2,35,77,34,51,44,2,3,4,11,2,3,4,100};
-	int z = partition(arr,30);
+	int arr[30] = {7,2,3,4,7,6,1,8,9,2,11,12,13,55,44,4,2,35,77,34,51,44,2,3,4,11,2,3,4,100};
+	partsort(arr,0,30);
 	for (int j = 0; j < 30; j++)
 	{
 		printf("This is arr[%i] :  %i\n",j,arr[j]);
 	}
-	printf("\nThis is Location: %i",z);
-
+	
 	return 0;
 }
