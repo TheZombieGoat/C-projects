3c58405c12aebe4ee13d5ec1823a800cca39678f
TheZombieGoat <98498075+TheZombieGoat@users.noreply.github.com>
2022-06-07 19:48:38 -0400

    Updated algorithm using Sieve of Eratosthenes
    Faster Runtime

--- prime.c
+++ prime.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-bool isPrime(int n)
-{    
-    for(int i = 2; (i*i) <= n; i++){
-        if(n % i == 0)
-            return false;
-    }
-    return true;
-}
-
 int countPrimes(int n){
     if(n <= 2)
         return 0;
     int count = 0;
-    int i;
-    for(i = 1; i < n; i+=2){
-        if(isPrime(i) == true)
+    bool *arr = malloc(sizeof(int)*n);
+    for(int i = 2; i < n; i++){
+        arr[i] = true;
+    }
+    for(int i = 2; i*i < n; i++){
+        if(!arr[i])
+            continue;
+        for(int j = i*i; j < n; j+=i){
+            arr[j] = false;
+        }
+    }
+    for(int i = 2; i < n; i++){
+        if(arr[i])
             count++;
     }
+    free(arr);
     return count;
 }
